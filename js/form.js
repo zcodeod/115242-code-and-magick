b621f24b4cd1e01c3035f23e2f99cf163564aae6
zcodeod <zcode@mail.ua>
2016-02-08 13:38:30 +0200

    Задание 1
    3 модуль

--- js/form.js
+++ js/form.js
@@ -14,4 +14,43 @@
     evt.preventDefault();
     formContainer.classList.add('invisible');
   };
+
+})();
+
+(function() {
+  var form = document.querySelector('.overlay.review-form');
+  var markInput = form.elements['review-mark'];
+  var nameInput = form.elements['review-name'];
+  var textInput = form.elements['review-text'];
+  var notifyFields = form.querySelector('.review-fields');
+  var notifyName = form.querySelector('.review-fields-name');
+  var notifyText = form.querySelector('.review-fields-text');
+  var button = form.querySelector('.review-submit');
+
+  nameInput.required = true;
+
+  var validate = function() {
+    textInput.required = +markInput.value < 3;
+
+    var nameValid = nameInput.validity.valid;
+    var textValid = textInput.validity.valid;
+    var formValid = nameValid && textValid;
+
+    notifyFields.style.visibility = !formValid ? 'visible' : 'hidden';
+    notifyName.style.visibility = !nameValid ? 'visible' : 'hidden';
+    notifyText.style.visibility = !textValid ? 'visinle' : 'hidden';
+
+    button.disabled = !formValid;
+
+  };
+
+  validate();
+
+  nameInput.oninput = validate;
+  textInput.oninput = validate;
+
+  for ( var i = 0; i < markInput.length; ++i ) {
+    markInput[i].onclick = validate;
+  }
+
 })();

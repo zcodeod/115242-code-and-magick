432a7e70ae04c4f55830439e49e0ef50208d1377
zcodeod <zcode@mail.ua>
2016-02-22 12:56:16 +0200

    Задание выполнено
    полностью

--- js/reviews.js
+++ js/reviews.js
@@ -1,36 +1,121 @@
 'use strict';
 
 var container = document.querySelector('.reviews-list');
-var element = document.querySelector('.review-template');
 var reviewsFilter = document.querySelector('.reviews-filter');
 var template = document.getElementById('review-template');
-reviewsFilter.classList.add('invisible');
 
-// 1. Перебираем все элементы в структуре данных
 
-/* global reviews */
+var filters = document.querySelector('.reviews-filter');
+var activeFilter = 'filter-all';
+var reviews = [];
 
-reviews.forEach(function(review) {
-  element = getElementFromTemplate(review);
-  container.appendChild(element);
+var filterItems = document.querySelectorAll('.reviews-filter-item');
+for ( var i = 0; i < filterItems.length; i++) {
+  filterItems[i].onclick = function(evt) {
+    var clickedElementId = evt.target.previousElementSibling.id;
+    setActiveFilter(clickedElementId);
+  };
+}
 
-});
+getReviews();
 
-reviewsFilter.classList.remove('invisible');
 
-// 2. Для каждого элемента создаем DOM-элемент на основе шаблона
+function renderReviews(reviewsRender) {
+  container.innerHTML = '';
+  var fragment = document.createDocumentFragment();
+  reviewsRender.forEach(function(review) {
+    var element = getElementFromTemplate(review);
+    fragment.appendChild(element);
+  });
 
-/**
-* @param {Object} data
-* @return {Element}
-*/
+  container.appendChild(fragment);
+}
+
+
+// Отрисовка списка отелей
+
+function setActiveFilter(id) {
+  if (activeFilter === id) {
+    return;
+  }
+
+
+// Фильтруем список отзывов
+
+  var filteredReviews = reviews.slice(0);
+
+  switch (id) {
+    case 'reviews-good':
+      filteredReviews = filteredReviews.filter(function(a) {
+        return a.rating >= 3;
+      });
+
+      filteredReviews = filteredReviews.sort(function(a, b) {
+        return b.rating - a.rating;
+      });
+      break;
+
+    case 'reviews-bad':
+      filteredReviews = filteredReviews.filter(function(a) {
+        return a.rating <= 2;
+      });
+
+      filteredReviews = filteredReviews.sort(function(a, b) {
+        return b.rating - a.rating;
+      });
+      break;
+
+    case 'reviews-popular':
+
+      filteredReviews = filteredReviews.sort(function(a, b) {
+        return b.review_usefulness - a.review_usefulness;
+      });
+      break;
+
+    case 'reviews-recent':
+      filteredReviews = filteredReviews.filter(function(a) {
+        var twoWeeksAgo = new Date() - 14 * 24 * 3600 * 1000;
+        return a.date <= twoWeeksAgo;
+      });
+      filteredReviews = filteredReviews.sort(function(a, b) {
+        return Date.parse(a.date) - Date.parse(b.date);
+      });
+      break;
+  }
+
+  renderReviews(filteredReviews);
+}
+
+
+// Загрузка списка отелей
+
+function getReviews() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', '//o0.github.io/assets/json/reviews.json');
+  xhr.onload = function(evt) {
+    var rawData = evt.target.response;
+    reviews = JSON.parse(rawData);
+    container.classList.remove('reviews-list-loading');
+
+    renderReviews(reviews);
+
+    reviewsFilter.classList.remove('invisible');
+  };
+
+  xhr.onerror = function() {
+    container.classList.add('reviews-load-failure');
+  };
+
+  container.classList.add('reviews-list-loading');
+  reviewsFilter.classList.add('invisible');
+
+  xhr.send();
+}
+
+
+// Загружаем данные
 
 function getElementFromTemplate(data) {
-
-	/**
-	var element = template.content.children[0].cloneNode(true);
-	return element;
-	*/
 
   if ('content' in template) {
     var templateElement = template.content.children[0].cloneNode(true);
@@ -56,7 +141,7 @@ function getElementFromTemplate(data) {
     templateElement.replaceChild(avatarImage, authorImage);
   };
 
-// 4. Добавляем обработку возможных ошибок
+//  Добавляем обработку возможных ошибок
 
   avatarImage.onerror = function() {
     templateElement.classList.add('review-load-failure');
